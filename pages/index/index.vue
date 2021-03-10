c1131489da5a6eedce0ad5fd9acd36ddbb28ca9e
Your Name <2426611417@qq.com>
2021-03-10 10:30:11 +0800

    完成基础的tabBar

---
+++ pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="">
+    <view class="" v-for="item in lbList">
+      {{item}}
+    </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				lbList:[]
+			}
+		},
+		onLoad() {
+    this.getLb()
+		},
+		methods: {
+      getLb(){
+        uni.request({
+          url:"http://localhost:8082/api/getlunbo",
+          success:function(res){
+            console.log(res.data)
+            this.lbList = res.data.message
+          }
+        })
+      }
+		}
+	}
+</script>
+
+<style>
+	
+</style>
